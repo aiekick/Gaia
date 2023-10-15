d3cfb8cc6d3c63a161b2d0df3454a6730d81103b
aiekick <aiekick@funparadigm.com>
2023-10-15 23:59:35 +0200

    -

--- cmake/spirv_tools.cmake
+++ cmake/spirv_tools.cmake
@@ -1,4 +1,4 @@
-add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/SPIRV-Tools)
+add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/SPIRV-Tools EXCLUDE_FROM_ALL)
 set_target_properties(SPIRV-Tools PROPERTIES FOLDER 3rdparty/Static/glslang)
 set(SPIRV_TOOLS_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/3rdparty/SPIRV-Tools/include)
 set(SPIRV_TOOLS_LIBRARIES SPIRV-Tools SPIRV-Tools-opt SPIRV-Tools SPIRV-Tools-link)
